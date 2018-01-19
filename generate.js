d6621c0e023d99b74ade14a68eed2974de046685
Steve Gehrman <sgehrman@gmail.com>
2018-01-18 23:23:37 -0800

    threaded version

--- generate.js
+++ generate.js
@@ -1,36 +1,47 @@
-const StellarSdk = require('stellar-sdk');
-const name = process.argv.slice(2)[0] || '';
+var Worker = require("tiny-worker")
+const StellarSdk = require('stellar-sdk')
 
-if(name.length === 0) {
-  console.log('Usage: node generate [string]');
-  process.exit();
+const name = process.argv.slice(2)[0] || ''
+
+if (name.length === 0) {
+  console.log('Usage: node generate [string]')
+  process.exit()
 }
 
-if(name.length === 5) {
-  console.warn('Warning: The string you provided is 5 characters this might take a while..');
+if (name.length === 5) {
+  console.warn('Warning: The string you provided is 5 characters this might take a while..')
 }
 
-if(name.length === 6) {
-  console.warn('Warning: The string you provided is 6 characters, it might take days to find your keypair');
+if (name.length === 6) {
+  console.warn('Warning: The string you provided is 6 characters, it might take days to find your keypair')
 }
 
-if(name.length >= 7) {
-  console.log('Aborting: The string you provided is 7 characters or longer, it will take too long to find your key.');
-  process.exit();
+if (name.length >= 7) {
+  console.log('Aborting: The string you provided is 7 characters or longer, it will take too long to find your key.')
+  process.exit()
 }
 
-console.log(`Trying to find public key ending with: ${name}`);
-let tries = 0;
-let keypair = StellarSdk.Keypair.random();
+console.log(`Trying to find public key ending with: ${name}`)
+const numWorkers = 10
+const workerTasks = []
+const upperCaseName = name.toUpperCase(name)
 
-while (keypair.publicKey().slice(-name.length) !== name.toUpperCase()) {
-  tries++;
-  keypair = StellarSdk.Keypair.random();
-  if(tries%1000 === 0) {
-    console.log('tries', tries);
-  }
+const workerListener = function(e) {
+  const secretKey = e.data
+  const keypair = StellarSdk.Keypair.fromSecret(secretKey)
+
+  console.log(`public key: ${keypair.publicKey()}`)
+  console.log(`secret key: ${keypair.secret()}`)
+
+  workerTasks.forEach(function(worker) {
+    worker.terminate()
+  })
 }
 
-console.log(`Found Stellar keypair in ${tries} tries`);
-console.log(`public key: ${keypair.publicKey()}`);
-console.log(`secret key: ${keypair.secret()}`);
+for (let i = 0; i < numWorkers; i++) {
+  const worker = new Worker('worker.js')
+  workerTasks.push(worker)
+
+  worker.onmessage = workerListener
+  worker.postMessage(upperCaseName)
+}
